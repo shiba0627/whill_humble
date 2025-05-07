faa169bc7c84e5a1f412a6ef58303d888285f17c
shiba0627 <1e8ec3@gmail.com>
2025-05-08 04:12:30 +0900

    一旦動いた

--- src/read_joy_package/read_joy_package/read_joy_node.py
+++ src/read_joy_package/read_joy_package/read_joy_node.py
@@ -1,5 +1,39 @@
-def main():
-    print('Hi from read_joy_package.')
+#/joyを購読し、走行履歴を作成・保存するノードをつくりたいが、今はパブリッシャ練習コード
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String#トピックの型
+
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('minimal_publisher')#ノード名
+        self.publisher_ = self.create_publisher(String, 'topic', 10)#キューサイズ10の'topic'をパブリッシュ
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)#コールバックタイマ, 0.5s毎にtimer_callbackを呼ぶ
+        self.i = 0#コールバックカウンタ
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = 'Hello World: %d' % self.i
+        self.publisher_.publish(msg)#パブリッシュ
+        self.get_logger().info('Publishing: "%s"' % msg.data)#コマンドラインに出力
+        self.i += 1
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPublisher()#ノードをインスタンス化
+
+    rclpy.spin(minimal_publisher)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
